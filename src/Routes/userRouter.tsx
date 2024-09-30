96aa29f7a80899d91939748ec34844b97858dbad
Mohammed Shibil PV <muhammedshibilpv77@gmail.com>
2024-09-30 14:11:44 +0530

    protected router

--- src/Routes/userRouter.tsx
+++ src/Routes/userRouter.tsx
@@ -1,18 +1,21 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes,Navigate, Route, useLocation } from "react-router-dom";
 import Auth from "../pages/auth";
 import NavBar from "../layout/navbar/navBar";
 import Dashboard from "../pages/dasboard";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 
 const UserRouter = () => {
     const location = useLocation();
+    const isLogin=useSelector((config:RootState)=>config.user.isAuthenticated)
     const showNavBar = location.pathname !== "/login";
 
     return (
         <>
             {showNavBar && <NavBar />}
             <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/login" element={<Auth />} />
+                <Route path="/" element={isLogin ? <Dashboard /> : <Navigate to="/login" />} />
+                <Route path="/login" element={isLogin ? <Navigate to="/" /> : <Auth />} />
             </Routes>
         </>
     );
